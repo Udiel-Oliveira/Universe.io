60c6593ce930dd54a8af3cc5430e51954197c486
Udiel-Oliveira <udiel.inaciosilva@gmail.com>
2023-12-06 20:18:46 -0300

    Reponsividade

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+
+});
+
+
+
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
